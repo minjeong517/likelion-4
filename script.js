cbdfae25387924f22a63ddf70141f1a2e83a7653
MinJeong <mineun0517@naver.com>
2025-04-11 19:26:45 +0900

    이벤트리스너 실습

---
+++ script.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector("#btn");
+
+const handleClick1 = () => {
+  console.log("첫 번째 함수");
+};
+
+const handleClick2 = () => {
+  console.log("두 번째 함수");
+};
+
+btn.addEventListener("click", handleClick1);
+btn.addEventListener("click", handleClick2);
+
+// 무명 함수는 제거 불가
+btn.removeEventListener("click", handleClick1);
